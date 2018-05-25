1a5e9b27de154bbf08eb4a5f3566824766ced7b7
dagoch <dagoch@gmail.com>
2018-05-24 23:37:11 -0400

    added folders; SampleScene and Rotate script

---
+++ Assets/Assignments/Assignment_01/Scripts/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    // properties
+    // Transform transform
+    // GameObject gameObject
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //Debug.Log(transform.eulerAngles);
+
+        // calculate rotation for each frame
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+	}
+}
