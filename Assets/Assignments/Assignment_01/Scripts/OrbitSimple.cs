01a0cd5cf3c56aa24315fdd6089e4b60e6f2d4e7
dagoch <dagoch@gmail.com>
2018-05-29 16:00:48 -0400

    working on demo orbit scripts

---
+++ Assets/Assignments/Assignment_01/Scripts/OrbitSimple.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitSimple : MonoBehaviour {
+
+    public Transform centralObject;
+
+    public Rigidbody myBody;
+
+
+    public float initForce = 100.0f;
+
+    public float dist = 0;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        myBody = GetComponent<Rigidbody>();
+        myBody.AddForce(transform.right * initForce);
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 inward = centralObject.position - transform.position;
+        dist = inward.magnitude;
+        inward.Normalize(); // get direction only
+
+        myBody.AddForce(inward * 10 / dist);
+
+        speed = myBody.velocity.magnitude;
+	}
+}
