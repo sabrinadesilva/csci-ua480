48258ca0ab631624a29fc2161f1cb98d7b7a8015
dagoch <dagoch@gmail.com>
2018-06-01 15:16:31 -0400

    In-class examples from 5/31

--- Assets/Assignments/Assignment_02/_A02_Master/Scripts/Orbit.cs
+++ Assets/Assignments/Assignment_02/_A02_Master/Scripts/Orbit.cs
@@ -6,30 +6,39 @@ namespace A02Examples
 {
     public class Orbit : MonoBehaviour
     {
+        // These are set in the Inspector
         public Vector3 startDirection;
-        public float gravity;
 
         public Transform centralBody;
+
+        // These are set in Start
         float centralMass;
         Rigidbody rb;
 
         // Use this for initialization
         void Start()
         {
+            // Get my rigidbody
             rb = GetComponent<Rigidbody>();
-
+            // Give me an initial push 
             rb.AddForce(startDirection);
+
+            // get the mass of the object I'm orbiting
             centralMass = centralBody.GetComponent<Rigidbody>().mass;
         }
 
         void FixedUpdate()
         {
+            // Find the direction and distance to the object I'm orbiting
             Vector3 dir = centralBody.position - transform.position;
-            float dist = dir.sqrMagnitude;
-
+            // Get the distance squared
+            float distSqr = dir.sqrMagnitude;
+            // Get the direction (with magnitude of 1)
             dir.Normalize();
 
-            rb.AddForce((rb.mass + centralMass)/dist * dir);
+            // Now accelerate me towards the object I'm orbiting,
+            // with a simulation of gravity
+            rb.AddForce((rb.mass + centralMass)/distSqr * dir);
         }
     }
 }
--- Assets/Assignments/Assignment_02/_A02_Master/Scripts/Push.cs
+++ Assets/Assignments/Assignment_02/_A02_Master/Scripts/Push.cs
@@ -2,21 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Push : MonoBehaviour {
+namespace A02Examples
+{
+    public class Push : MonoBehaviour
+    {
 
-    public float speed = 10.0f;
+        public float speed = 10.0f;
 
-    Rigidbody rb;
+        Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
 
+            // Give this object one push, at the start of the game:
+            // this will make the object move with a constant speed, forever
+            rb.AddForce(Vector3.right * speed);
 
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        rb.AddForce(Vector3.right * speed);
-	}
+        }
+
+        void FixedUpdate()
+        {
+            // Keep pushing for every physics update: this
+            //  will continuously accelerate the object
+            rb.AddForce(Vector3.right * speed);
+        }
+    }
 }
