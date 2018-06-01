4003488eb94a01ea7d1b7016b529b5841cc322ba
dagoch <dagoch@gmail.com>
2018-06-01 12:45:23 -0400

    moved my scratch code and scenes into dag dir

---
+++ Assets/dag/BallController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float speed = 1.0f;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(direction * speed);
+    }
+}
---
+++ Assets/dag/SimpleForces.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleForces : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
