ccf91f659171cbb11dc488313e1faa12596abcf6
David Lobser <davidlobser@yahoo.com>
2018-06-06 14:01:48 -0400

    raycasting example for assignment 2 (#36)

---
+++ Assets/Assignments/Assignment_02/_A02_Master/Scripts/Shake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A02Examples
+{
+    public class Shake : MonoBehaviour
+    {
+
+        public float speed;
+        public float amount;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.localEulerAngles = new Vector3(0, Mathf.Sin(Time.time * speed) * amount, 0);
+        }
+    }
+}
