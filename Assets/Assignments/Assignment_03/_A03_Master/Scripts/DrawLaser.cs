244e7239ece9000b7e42684160c91c5ace2afb52
dagoch <dagoch@gmail.com>
2018-06-12 17:09:19 -0400

    add Assignment_03 folder; move projectsettings and editorbuildsettings out of repo

---
+++ Assets/Assignments/Assignment_03/_A03_Master/Scripts/DrawLaser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A03Examples { 
+    public class DrawLaser : MonoBehaviour
+    {
+
+        LineRenderer lineRenderer;
+        bool drawLine;
+
+        void Start()
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+
+      
+        public void DrawLine(Vector3 A, Vector3 B){
+            drawLine = true;
+            if (lineRenderer != null)
+            {
+                lineRenderer.positionCount = 2;
+                lineRenderer.SetPosition(0, A);
+                lineRenderer.SetPosition(1, B);
+            }
+        }
+
+        public void DontDraw(){
+            if(lineRenderer!=null)
+                lineRenderer.positionCount = 0;
+        }
+    }
+}
---
+++ Assets/Assignments/Assignment_03/_A03_Master/Scripts/RaycastInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A03Examples
+{
+    public class RaycastInput : MonoBehaviour
+    {
+
+        public GameObject explosion;
+
+        void Update()
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+                Debug.Log(hit.transform.gameObject.name);
+                if(Input.GetMouseButton(0)){
+                    GameObject g = Instantiate(explosion, objectHit.position, Quaternion.identity);
+                    Destroy(objectHit.gameObject);
+                    Destroy(g, 1f);
+                }
+            }
+
+        }
+    }
+}
