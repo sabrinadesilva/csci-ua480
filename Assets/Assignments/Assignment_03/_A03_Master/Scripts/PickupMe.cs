77a4211051fa49fd8d5dcb9e2a920437f4ab0017
dagoch <dagoch@gmail.com>
2018-06-14 17:04:53 -0400

    example for class 6/14

---
+++ Assets/Assignments/Assignment_03/_A03_Master/Scripts/PickupMe.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A03Examples
+{
+    public class PickupMe : MonoBehaviour
+    {
+        public bool grabbed = false;
+        Rigidbody myRb;
+        StrobeSelected strobe;
+
+        // Use this for initialization
+        void Start()
+        {
+            myRb = GetComponent<Rigidbody>();
+            strobe = GetComponent<StrobeSelected>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void PickupOrDrop()
+        {
+            if (grabbed)
+            {  // now drop it
+                transform.parent = null;
+                grabbed = false;
+                myRb.isKinematic = false;  //    .useGravity = true;
+                strobe.trigger = false;
+            }
+            else
+            {
+                transform.parent = Camera.main.transform;
+                grabbed = true;
+                strobe.trigger = true;
+                myRb.isKinematic = true; //  .useGravity = false;
+            }
+        }
+    }
+}
---
+++ Assets/Assignments/Assignment_03/_A03_Master/Scripts/StrobeSelected.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A03Examples
+{
+    public class StrobeSelected : MonoBehaviour
+    {
+
+        public bool trigger = false;
+        public Color strobeColor = Color.red;
+        public float strobeSpeed = 12;
+
+        Color initColor;
+        bool prevTrigger = false;
+
+        void Start()
+        {
+            initColor = this.GetComponent<MeshRenderer>().material.color;
+        }
+
+        void Update()
+        {
+
+            HandleTrigger();
+        }
+
+        void HandleTrigger()
+        {
+
+            if (trigger)
+            {
+                Strobe();
+            }
+
+            if (!trigger && prevTrigger)
+            {
+                this.GetComponent<MeshRenderer>().material.color = initColor;
+            }
+
+            prevTrigger = trigger;
+
+        }
+
+        void Strobe()
+        {
+            this.GetComponent<MeshRenderer>().material.color =
+                Color.Lerp(initColor, strobeColor, (Mathf.Sin(Time.time * strobeSpeed) + 1) * .5f);
+
+        }
+    }
+}
