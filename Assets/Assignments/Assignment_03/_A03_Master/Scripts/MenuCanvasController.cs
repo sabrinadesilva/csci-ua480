d5473485ac3412a767fb181ac20a7ef22e0c8d40
SiyuanQ <1833836175@qq.com>
2018-06-17 15:56:29 -0400

    UI Canvas Example for class 6/14 (#68)

---
+++ Assets/Assignments/Assignment_03/_A03_Master/Scripts/MenuCanvasController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Canvas))]
+public class MenuCanvasController : MonoBehaviour {
+    private Canvas _canvas;
+    private float _distanceToCamera;
+
+	private void Start()
+	{
+        _canvas = GetComponent<Canvas>();
+        _canvas.enabled = false;
+
+        //Get the initial distance between the canvas and the camera, and project it on the camera's forward direction
+        Vector3 dis = Camera.main.transform.position - transform.position;
+        _distanceToCamera = Vector3.Project(dis, Camera.main.transform.forward).magnitude;
+	}
+
+	public void Show () {
+        _canvas.enabled = true;
+        transform.position = Camera.main.transform.position + Camera.main.transform.forward * _distanceToCamera;
+        transform.forward = Camera.main.transform.forward;
+    }
+
+    public void Hide() {
+        _canvas.enabled = false;
+    }
+}
