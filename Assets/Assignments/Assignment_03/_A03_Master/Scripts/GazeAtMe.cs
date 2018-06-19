edba3b29914fca1ea3626258a7ae1115efe5a441
david lobser <davidlobser@yahoo.com>
2018-06-19 17:19:19 -0400

    added gaze interaction for assignment 3

---
+++ Assets/Assignments/Assignment_03/_A03_Master/Scripts/GazeAtMe.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A03Examples
+{
+   
+    public class GazeAtMe : MonoBehaviour
+    {
+        Rigidbody myRb;
+        public Color selectColor = Color.red;
+        Color initialColor;
+        float counter = 0;
+        MeshRenderer meshRenderer;
+        IEnumerator coroutine;
+
+        void Start()
+        {
+            meshRenderer = GetComponent<MeshRenderer>();
+            initialColor = meshRenderer.material.color;
+            myRb = GetComponent<Rigidbody>();
+
+        }
+
+        public void GazeAndPop()
+        {
+            coroutine = Gaze();
+            StartCoroutine(coroutine);
+        }
+
+		
+		IEnumerator Gaze(){
+            counter = 0;
+            while (counter < 1)
+            {
+                Debug.Log("hi");
+                counter += Time.deltaTime;
+                meshRenderer.material.color = Color.Lerp(initialColor, selectColor, counter / 1f);
+                yield return null;
+            }
+            myRb.AddForce(Vector3.up * 300 + Random.insideUnitSphere * .25f);
+            myRb.AddTorque(Random.insideUnitSphere * 10);
+            Reset();
+        }
+
+		public void Reset()
+		{
+            StopCoroutine(coroutine);
+            meshRenderer.material.color = initialColor;
+            counter = 0;
+		}
+	}
+}
