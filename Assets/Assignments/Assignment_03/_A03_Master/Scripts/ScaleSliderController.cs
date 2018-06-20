b541bfcee5aae896a9d5e304d85d4699a9d0b178
SiyuanQ <siyuan.qiu1@gmail.com>
2018-06-20 18:32:32 -0400

    Slider UI Example with Gaze Interaction

--- Assets/Assignments/Assignment_03/_A03_Master/Scripts/ScaleSliderController.cs
+++ Assets/Assignments/Assignment_03/_A03_Master/Scripts/ScaleSliderController.cs
@@ -5,39 +5,39 @@ using UnityEngine.UI;
 
 namespace A03Examples
 {
+    [RequireComponent(typeof(Slider))]
     public class ScaleSliderController : MonoBehaviour
     {
         [HideInInspector]
         public Transform ControllingTransform;
 
-        public float MaxAngle = 45.0f;
+        [Tooltip("The scale will change between 1 - range and 1 + range")]
+        [Range(0.1f, 0.9f)]
+        public float ScaleChangeRange = 0.5f;
 
         private Slider _slider;
-        private float _intialCameraRotationY;
         private Vector3 _initialLocalScale;
-        private bool _buttonReleased;
 
-        private void Start()
-        {
-            _slider = GetComponent<Slider>();
-            //The initial value is 0.5 for 1*1*1 scale in transform
-            _slider.value = 0.5f;
+        public void ChangeObjectSize () {
+            if (ControllingTransform != null)
+            {
+                ControllingTransform.localScale = _initialLocalScale * Mathf.Lerp(1.0f - ScaleChangeRange, 1.0f + ScaleChangeRange, _slider.value);
+            }
         }
 
-        private void OnEnable()
-        {
-            if (MenuCanvasController.Instance == null)
-            {
-                return;
-            }
+		private void Start()
+		{
+            _slider = GetComponent<Slider>();
+		}
 
-            _intialCameraRotationY = ConvertAngleIn180(Camera.main.transform.rotation.eulerAngles.y);
-            if (MenuCanvasController.Instance.ControllingObject != null)
+		private void OnEnable()
+        {
+            if (MenuCanvasController.Instance != null && MenuCanvasController.Instance.ControllingObject != null)
             {
                 ControllingTransform = MenuCanvasController.Instance.ControllingObject.transform;
                 _initialLocalScale = ControllingTransform.localScale;
+                _slider.value = 0.5f;
             }
-            _buttonReleased = false;
         }
 
         private void OnDisable()
@@ -46,62 +46,5 @@ namespace A03Examples
             _initialLocalScale = Vector3.one;
         }
 
-        private float ConvertAngleIn180 (float angle) 
-        {
-            if (angle > 180.0f)
-            {
-                return angle - 360.0f;
-            }
-            else
-            {
-                return angle;
-            }
-        }
-
-        private float GetCurrentDeltaRotationFactor()
-        {
-            float currentCameraRotationY = ConvertAngleIn180(Camera.main.transform.rotation.eulerAngles.y);
-            float ClampedDeltaAngle = Mathf.Clamp((currentCameraRotationY - _intialCameraRotationY), -MaxAngle, MaxAngle);
-            return ClampedDeltaAngle / (MaxAngle * 2) + 0.5f;
-        }
-
-        private bool CheckIfClickButton()
-        {
-            if (Input.GetMouseButton(0))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        void LateUpdate()
-        {
-            if (ControllingTransform != null)
-            {
-                float currenDeltaRotationFactor = GetCurrentDeltaRotationFactor();
-                _slider.value = currenDeltaRotationFactor;
-                ControllingTransform.localScale = _initialLocalScale * Mathf.Lerp(0.5f, 1.5f, currenDeltaRotationFactor);
-
-                if (CheckIfClickButton())
-                {
-                    if (_buttonReleased)
-                    {
-                        _buttonReleased = false;
-                        MenuCanvasController.Instance.Hide();
-                    }
-                }
-                else
-                {
-                    if (!_buttonReleased)
-                    {
-                        _buttonReleased = true;
-                    }
-                }
-            }
-
-        }
     }
 }
