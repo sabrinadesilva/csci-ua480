e06968fdee740d690989a095b800f621e99f13e6
SiyuanQ <siyuan.qiu1@gmail.com>
2018-06-21 14:49:47 -0400

    Assignment_04 Player Movement Examples

---
+++ Assets/Assignments/Assignment_04/_A04_Master/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A04Examples
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public static PlayerController Instance;
+
+		private void Awake()
+		{
+            //Singleton
+            if (Instance == null) {
+                Instance = this;
+            } else if (Instance != this) {
+                Destroy(this);
+            }
+		}
+
+        public void MoveToPosition(Vector3 des, float time)
+        {
+            StopAllCoroutines();
+            StartCoroutine(MoveToPositionGradually(des, time));
+        }
+
+        private IEnumerator MoveToPositionGradually(Vector3 des, float time) {
+            float t = 0.0f;
+            Vector3 initialPos = transform.position;
+            while (t < time) {
+                t += Time.smoothDeltaTime;
+                transform.position = initialPos + (des - initialPos) * Mathf.Lerp(0.0f, 1.0f, t / time);
+                yield return null;
+            }
+            transform.position = des;
+        }
+    }
+}
